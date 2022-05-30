e3a3d7d290b2ad00fb962dd8b184c376dd3d7de6
bruk19 <brukteshome21@gmail.com>
2022-05-31 01:34:23 +0300

    Add linters fix

--- modules/luxon.js
+++ modules/luxon.js
@@ -7037,4 +7037,3 @@ function friendlyDateTime(dateTimeish) {
 const VERSION = "2.4.0";
 
 export { DateTime, Duration, FixedOffsetZone, IANAZone, Info, Interval, InvalidZone, Settings, SystemZone, VERSION, Zone };
-//# sourceMappingURL=luxon.js.map
