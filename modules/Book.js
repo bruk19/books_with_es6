3b97418d8ebbd5eaad4fba89f610bc9fb044b861
bruk19 <brukteshome21@gmail.com>
2022-05-31 00:54:35 +0300

    Refactor to use ES6 arrow fn in class methods

--- modules/Book.js
+++ modules/Book.js
@@ -1,19 +1,19 @@
 export default class book {
-    constructor(title, author, id) {
-      this.title = title;
-      this.author = author;
-      this.id =id;
-    }
-  
-    createNode = () => {
-        const bookNode = document.createElement('li')
-        const btn =document.createElement('button')
-        btn.textContent = 'Remove';
-  
-        bookNode.innerHTML = `
-        <span>'${this.title}' by ${this.author}</span>`;
-        bookNode.append(btn);
-  
-        return { bookNode, btn}
-    }
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id =id;
   }
+  
+  createNode = () => {
+    const bookNode = document.createElement('li')
+    const btn =document.createElement('button')
+    btn.textContent = 'Remove';
+
+    bookNode.innerHTML = `
+    <span>'${this.title}' by ${this.author}</span>`;
+    bookNode.append(btn);
+
+    return { bookNode, btn}
+  }
+}
